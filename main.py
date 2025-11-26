d0e2e7f3a1060533733090df5f13c70ae69f5382
mintcolon3 <143480965+mintcolon3@users.noreply.github.com>
2025-11-26 19:42:56 +0000

    remove, no perms?, RandomPick yuri, $ping @evilmintcolon3

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ bsky = AsyncClient()
 
 intents = discord.Intents.all()
 bot = commands.Bot(
-    command_prefix="$",
+    command_prefix=["$", "remove"],
     intents=intents
 )
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.reply("no perms?")
 
 @bot.event
 async def on_ready():
@@ -35,12 +40,23 @@ async def on_ready():
     await bot.change_presence(activity=discord.CustomActivity(name="girls kissing"), status=discord.Status.online)
 
 @bot.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if random.randint(1,50) == 1: await message.add_reaction("<:osagebwaa:1427902721410732032>")
 
     if message.content.lower() == "$yuri" and message.webhook_id != None:
         await get_yuri(message.channel, 1)
         return
+    
+    if message.author.id in [1440352198709088306, 1417180333438144525] and message.channel.id != 1427691861216329769: # RandomPick, bwaabot ; #yuri
+
+        while len(message.embeds) == 0:
+            await asyncio.sleep(2)
+        
+        if message.embeds[0].title == "🎨 Random Image!":
+            tags = message.embeds[0].description[5:]
+            if "yuri" in tags:
+                yuri_channel = bot.get_channel(auth.YURI_CHANNEL)
+                await message.forward(yuri_channel)
 
     await bot.process_commands(message)
 
@@ -79,5 +95,15 @@ async def load_commands(ctx):
     await bot.tree.sync()
     await ctx.channel.send("commands loaded")
 
+@bot.command()
+@commands.has_role(1427690676635570238)
+async def ping(ctx: commands.Context, target: discord.Member):
+    webhook = await ctx.channel.create_webhook(name=f"{target.display_name.lower()} pinger 3000")
+
+    for i in range(8):
+        await webhook.send(content=f"<@{target.id}>", avatar_url="https://cdn.discordapp.com/avatars/1435689439472124138/b94db1d7b2e07d92f37b52454138468e.png?size=1024")
+        await asyncio.sleep(1)
+    
+    await webhook.delete()
 
 bot.run(auth.TOKEN)
